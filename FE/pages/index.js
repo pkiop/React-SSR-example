b94dcf094f46504dbe8460d50167317c34b495ce
pkiop <pkiop@naver.com>
2021-01-30 23:43:27 +0900

    Feat: Add profile link in index

--- FE/pages/index.js
+++ FE/pages/index.js
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
+
+const ProfileLink = props => {
+  return(
+    <Link href={`/profile?name=${props.name}`}>
+      <div>
+        {props.name} 입니다.
+      </div>
+    </Link>
+  )
+}
 
 const Index = () => (
   <Layout>
     <p>index page</p>
+    <ProfileLink name="a"/> 
+    <ProfileLink name="b"/> 
+    <ProfileLink name="c"/> 
   </Layout>
 );
 
