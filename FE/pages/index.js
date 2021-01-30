498107244c3d65b43e0acf1375c2b84982c08b3f
pkiop <pkiop@naver.com>
2021-01-30 23:51:23 +0900

    Feat: Add new query method using as

--- FE/pages/index.js
+++ FE/pages/index.js
@@ -1,22 +1,20 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
-const ProfileLink = props => {
-  return(
-    <Link href={`/profile?name=${props.name}`}>
-      <div>
-        {props.name} 입니다.
-      </div>
+const ProfileLink = props => (
+  <div>
+    <Link href={`/p/profile`} as={`/p/${props.profile}`}>
+      <a>Go to {props.profile}'s profile</a>
     </Link>
-  )
-}
+  </div>
+);
 
 const Index = () => (
   <Layout>
-    <p>index page</p>
-    <ProfileLink name="a"/> 
-    <ProfileLink name="b"/> 
-    <ProfileLink name="c"/> 
+    <h1>Friends List</h1>
+    <ProfileLink profile="Jake" />
+    <ProfileLink profile="Peter" />
+    <ProfileLink profile="Yumi" />
   </Layout>
 );
 
